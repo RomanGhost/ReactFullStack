c87d697340486a1fb514989fa645a25fe47a11f8
Ghost <rildiarov@gmail.com>
2024-03-12 08:17:24 +0400

    sandbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,19 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 
 function App() {
-  const [bitcoinData, setBitcoinData] = useState({ price: null, lastUpdated: null });
-  const [etheriumData, setEtheriumData] = useState({ price: null, lastUpdated: null });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
-        const data = await response.json();
-        
-        const currentTime = new Date().toLocaleTimeString();
-
-        setBitcoinData({ price: data.bitcoin.usd, lastUpdated: currentTime });
-        setEtheriumData({ price: data.ethereum.usd, lastUpdated: currentTime });
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <header className="header">
-      <div>
-        <h2>p2p stock</h2>
-      </div>
-      <div className="prices">
-        <p>Bitcoin: ${bitcoinData.price} | Last Updated: {bitcoinData.lastUpdated}</p>
-        <span className="price-gap"></span>
-        <p>Ethereum: ${etheriumData.price} | Last Updated: {etheriumData.lastUpdated}</p>
-      </div>
-    </header>
+<>
+			<div className='app'>
+				<Header />
+				<Content />
+				<Footer />
+			</div>
+		</>
   );
 }
 
